0fa279e5964f47c2b1dfb4fff614e0de11c4f4f6
PeterMazzoli <Peter.mazzoli@gmail.com>
2017-02-01 15:40:41 -0500

    adding more

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'jEXEFrl6rMleVCxysR6vlt8jK',
+  consumer_secret: 'JGOdvcu7Qn2PMQYpocd8KzHumeCydvTiNBjtwjRVrJBl6nCs4X',
+  access_token_key: '826821520114741249-i7OpnEFsIopCSim2z1YGjSzgxJVGzbE',
+  access_token_secret: 'BEgbkCpyZNoE9mzxxNscACXueS4H2o5Oda89g2laQEbJ4',
 }
